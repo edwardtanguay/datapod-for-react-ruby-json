4aa082d4186e8cdcb2b5db9abe35e5dcb448ec2c
Edward Tanguay (windows) <edward@tanguay.info>
2025-04-25 14:12:07 +0200

    learn class in

--- scripts/commands/create_page.rb
+++ scripts/commands/create_page.rb
@@ -3,3 +3,4 @@ require_relative '../qtools/qdev'
 
 QDev.debug("inside create-page script")
 QCli.message("This will create a new page in the React site")
+
---
+++ scripts/developer/learn.rb
@@ -0,0 +1,11 @@
+require_relative '../qtools/qdev'
+
+class Learn
+	def ex_001
+		QDev.debug("ex001")
+	end
+
+	def ex_002
+		QDev.debug("ex002")
+	end
+end
