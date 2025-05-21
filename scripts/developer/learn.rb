7601b4873d35a44d1ba95a6da6aca9d5196f7eb8
Edward Tanguay (windows) <edward@tanguay.info>
2025-05-21 04:10:00 +0200

    notes

--- scripts/developer/learn.rb
+++ scripts/developer/learn.rb
@@ -8,6 +8,12 @@ class Learn
 
 	def ex_001
 		self.print_title("EX001: Create a file")
+		path_and_file_name = "../developer/output/test.txt"
+		File.open(path_and_file_name, "w") do |file|
+			file.puts("this is line 1")
+			file.puts("this is line 2")
+		end
+		QDev.debug("File #{path_and_file_name} created")
 	end
 
 	def ex_002
