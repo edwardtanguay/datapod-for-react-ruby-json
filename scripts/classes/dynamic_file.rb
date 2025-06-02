c59e8e145cc454d1fe36b7e18cad52046e60d49a
Edward Tanguay (windows) <edward@tanguay.info>
2025-06-02 22:31:02 +0200

    getting lines of a file

--- scripts/classes/dynamic_file.rb
+++ scripts/classes/dynamic_file.rb
@@ -1,9 +1,23 @@
+require_relative '../qtools/qfil'
+
 class DynamicFile
 	def initialize(pathAndFileName)
 		@pathAndFileName = pathAndFileName
+		self.init_state
+	end
+
+	def init_state
+		@lines = QFil.get_lines_from_file(@pathAndFileName)
 	end
 
 	def debug
-		puts "hello"
+		puts "@pathAndFileName = \"#{@pathAndFileName}\""
+		puts "@lines ="
+		puts "---"
+		@lines.each do |line|
+			puts line
+		end
+		puts "---"
 	end
+
 end
--- scripts/commands/create_page.rb
+++ scripts/commands/create_page.rb
@@ -2,6 +2,6 @@ require_relative '../qtools/qcli'
 require_relative '../qtools/qdev'
 require_relative '../classes/dynamic_file'
 
-dynamic_file = DynamicFile.new('test.txt')
+dynamic_file = DynamicFile.new('../../data/flashcards.txt')
 dynamic_file.debug
 
