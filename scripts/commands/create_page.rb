5162c095cc661ac161a5a3a85bb31128bdd4ad8b
Edward Tanguay (windows) <edward@tanguay.info>
2025-06-04 08:34:16 +0200

    add line before works

--- scripts/commands/create_page.rb
+++ scripts/commands/create_page.rb
@@ -5,5 +5,6 @@ require_relative '../classes/dynamic_file'
 # dfMain = DynamicFile.new('../../src/main.tsx')
 dfMain = DynamicFile.new('../../dev/mock/marker_test.txt')
 dfMain.add_line_before_marker('importPage', 'import { PageProducts } from "./pages/PageProducts.tsx";')
+dfMain.rerender_to_file
 dfMain.debug(1)
 
--- scripts/qtools/qfil.rb
+++ scripts/qtools/qfil.rb
@@ -1,20 +1,32 @@
 require_relative './qcli'
 
 module QFil
-  # Reads a file and returns its lines as an array of strings.
-  #
-  # Example:
-  # lines = QFil.get_lines_from_file('../data/flashcards.txt')
-  #
-  # @param file_path [String] Path to the file to read
-  # @return [Array<String>] Array of lines from the file, or empty array if file cannot be read
-  def self.get_lines_from_file(file_path)
-    begin
-      file_content = File.read(file_path)
-      file_content.split(/\r?\n/)
-    rescue => error
-      QCli.message("Error reading file at #{file_path}: #{error.message}", "error")
-      []
-    end
-  end
+	# Reads a file and returns its lines as an array of strings.
+	#
+	# Example:
+	# lines = QFil.get_lines_from_file('../data/flashcards.txt')
+	#
+	# @param file_path [String] Path to the file to read
+	# @return [Array<String>] Array of lines from the file, or empty array if file cannot be read
+	def self.get_lines_from_file(file_path)
+		begin
+			file_content = File.read(file_path)
+			file_content.split(/\r?\n/)
+		rescue => error
+			QCli.message("Error reading file at #{file_path}: #{error.message}", "error")
+			[]
+		end
+	end
+
+	def self.write_lines_to_file(file_path, lines)
+		begin
+			File.open(file_path, 'w') do |file|
+				lines.each { |line| file.puts(line) }
+			end
+			QCli.message("File written successfully to #{file_path}", "success")
+		rescue => error
+			QCli.message("Error writing to file at #{file_path}: #{error.message}", "error")
+		end
+	end
+
 end
