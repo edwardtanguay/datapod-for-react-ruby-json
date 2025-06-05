ce8fbd0f1fb3a869a77c3656e3c251bd8d7af56a
Edward Tanguay (windows) <edward@tanguay.info>
2025-06-05 07:24:42 +0200

    bef nav test

--- scripts/classes/page_builder.rb
+++ scripts/classes/page_builder.rb
@@ -10,15 +10,7 @@ class PageBuilder
 
 	def create_page
 		self.add_code_to_main_tsx	
-	end
-
-	def add_code_to_nav_tsx
-		@dfMain = DynamicFile.new('../../src/components/Nav.tsx')
-		@dfMain.add_variable('pageTitle', @page_title)
-		@dfMain.add_variable('pagePascal', @page_pascal)
-		@dfMain.add_variable('pageCamel', @page_camel)
-		@dfMain.add_block_before_marker('navEntryArea', "navEntry#{@page_pascal}", 'navEntryBlock')
-		@dfMain.rerender_to_file
+		self.add_code_to_nav_tsx	
 	end
 
 	def add_code_to_main_tsx
@@ -28,6 +20,15 @@ class PageBuilder
 		@dfMain.add_variable('pageCamel', @page_camel)
 		@dfMain.add_line_before_marker('importStatementArea', "importPage#{@page_pascal}", 'import { Page@@pagePascal } from "./pages/Page@@pagePascal.tsx";')
 		@dfMain.add_block_before_marker('routerEntryArea', "routerEntry#{@page_pascal}", 'routerEntryBlock')
+		@dfMain.rerender_to_file
+	end
+
+	def add_code_to_nav_tsx
+		@dfMain = DynamicFile.new('../../src/components/Nav.tsx')
+		@dfMain.add_variable('pageTitle', @page_title)
+		@dfMain.add_variable('pagePascal', @page_pascal)
+		@dfMain.add_variable('pageCamel', @page_camel)
+		@dfMain.add_block_before_marker('navEntryArea', "navEntry#{@page_pascal}", 'navEntryBlock')
 		@dfMain.rerender_to_file
 	end
 end
