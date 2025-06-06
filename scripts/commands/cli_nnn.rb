4ef888f444c126f6c37e05a8538020c9f6c0a2c9
Edward Tanguay (windows) <edward@tanguay.info>
2025-06-06 10:53:33 +0200

    FIX: Replaced emoticons and aligned spacing

---
+++ scripts/commands/cli_nnn.rb
@@ -0,0 +1,8 @@
+require_relative '../qtools/qcli'
+
+QCli.message("info", "info")
+QCli.message("error", "error")
+QCli.message("warning", "warning")
+QCli.message("doing", "doing")
+QCli.message("ball", "ball")
+QCli.message("check", "check")
