e95d67c84db26418df2c6c4cd3454acc14da3e82
Edward Tanguay (windows) <edward@tanguay.info>
2025-06-17 06:59:14 +0200

    FIX: Change create-page to snakecase

--- scripts/classes/page_manager.rb
+++ scripts/classes/page_manager.rb
@@ -10,6 +10,7 @@ class PageManager
 		@page_pascal = QStr.force_pascal_notation(page_title)
 		@page_camel = QStr.force_camel_notation(page_title)
 		@page_text = QStr.force_text_notation(page_title)
+		@page_snake = QStr.force_snake_notation(page_title)
 		@page_tsx_page_and_file_name = "src/pages/Page#{@page_pascal}.tsx"
 		@main_tsx_import_line_marker = "importPage#{@page_pascal}"
 		@main_tsx_router_block_marker = "routerEntry#{@page_pascal}"
@@ -58,6 +59,7 @@ class PageManager
 		@dfMain.add_variable('pageTitle', @page_title)
 		@dfMain.add_variable('pagePascal', @page_pascal)
 		@dfMain.add_variable('pageCamel', @page_camel)
+		@dfMain.add_variable('pageSnake', @page_snake)
 		@dfMain.add_line_before_marker('importStatementArea', @main_tsx_import_line_marker, 'import { Page@@pagePascal } from "./pages/Page@@pagePascal.tsx";')
 		@dfMain.add_block_before_marker('routerEntryArea', @main_tsx_router_block_marker, 'routerEntryBlock')
 		@dfMain.rerender_to_file
@@ -68,6 +70,7 @@ class PageManager
 		@dfMain.add_variable('pageTitle', @page_title)
 		@dfMain.add_variable('pagePascal', @page_pascal)
 		@dfMain.add_variable('pageCamel', @page_camel)
+		@dfMain.add_variable('pageSnake', @page_snake)
 		@dfMain.add_block_before_marker('navEntryArea', @nav_tsx_entry_block_marker, 'navEntryBlock')
 		@dfMain.rerender_to_file
 	end
